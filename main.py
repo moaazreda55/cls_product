31aec2259d0a22b2bf7113a68583203bb327e5e5
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-08-08 23:16:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import io
+import os
+import numpy as np
+from PIL import Image
+from infer import classifier_google, classifier_facebook, classifier_resnet
+
+
+app = FastAPI()
+
+
+@app.post('/predict/google')
+async def predict(file: UploadFile = File(...)):
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+    image.save("image.jpg")
+    preds = classifier_google("image.jpg")
+    return {"message":"Image uploaded successfully" ,"preds" : preds}
+
+
+@app.post('/predict/resnet50')
+async def predict(file: UploadFile = File(...)):
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+    image.save("image.jpg")
+    preds = classifier_resnet("image.jpg")
+    return {"message":"Image uploaded successfully" ,"preds" : preds}
+
+
+@app.post('/predict/facebook')
+async def predict(file: UploadFile = File(...)):
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+    image.save("image.jpg")
+    preds = classifier_facebook("image.jpg")
+    return {"message":"Image uploaded successfully" ,"preds" : preds}
