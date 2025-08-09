241c68016aee11d163c9924c5742f3318ea13ec8
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-08-09 22:32:29 +0300

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -4,16 +4,15 @@ import os
 os.makedirs("models", exist_ok=True)
 
 
-def download_model(file_id, name):
-   
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, f"models/{name}")
+def download_folder(folder_id, output_dir):
+    url = f"https://drive.google.com/drive/folders/{folder_id}"
+    gdown.download_folder(url, output=output_dir, quiet=False, use_cookies=False)
 
 
-# https://drive.google.com/drive/folders/10ctqnrEiQLrZ0XdTCo7u78XFHM670Eh8?usp=sharing
-download_model("10ctqnrEiQLrZ0XdTCo7u78XFHM670Eh8", "google")
-# https://drive.google.com/drive/folders/1drHBlYnZcFEuAKs6o_g7hWqdPQqV36Gc?usp=sharing
-download_model("1drHBlYnZcFEuAKs6o_g7hWqdPQqV36Gc", "resnet50")
-# https://drive.google.com/drive/folders/1Pi6TLH6bBi_oFKJeO7H1Vgb0JIK7VH_f?usp=sharing
-download_model("1Pi6TLH6bBi_oFKJeO7H1Vgb0JIK7VH_f", "facebook")
+download_folder("10ctqnrEiQLrZ0XdTCo7u78XFHM670Eh8", "models/google")
+
+download_folder("1drHBlYnZcFEuAKs6o_g7hWqdPQqV36Gc", "models/resnet50")
+
+download_folder("1Pi6TLH6bBi_oFKJeO7H1Vgb0JIK7VH_f", "models/facebook")
+
 
